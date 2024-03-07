d0453cd4dca73a6194d5449b32b48f084152b2c4
personaforhire <conoraskins104@gmail.com>
2024-03-07 23:29:25 +0000

    formatting changes and made count letter cleaner

--- main.py
+++ main.py
@@ -17,23 +17,15 @@ def count_letters():
                 letters.update({letter:letters[letter]+1})
             else:
                 letters[letter]= 1
-    return letters
+    return letters.items()
 
 def report():
     print(f"----Begin report for {path} ----\n{count_words()} words in this document \n")
-    for letter, count in sorted(count_letters().items()):
-        print(f" - The letter {letter} was used {count} mad right? - \n")
-    
-
-
-
+    for letter, count in sorted(count_letters()):
+        print(f" - The letter {letter} was used {count} times mad right? - \n")
 
 def main():
-   # print(openfile())
-   # count_words()
-   #count_letters()
     report()
 
 if __name__ == "__main__":
     main()
-
